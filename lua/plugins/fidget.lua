f3f7154a646d5ddbbf285f1d25b63c037d18929b
Trevor <treramey@gmail.com>
2024-03-25 08:50:20 -0500

    update config

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,15 +1,55 @@
 return {
-	{
-		"j-hui/fidget.nvim",
-		tag = "legacy",
-		event = { "BufEnter" },
-		config = function()
-			-- Turn on LSP, formatting, and linting status and progress information
-			require("fidget").setup({
-				text = {
-					spinner = "dots_negative",
-				},
-			})
-		end,
-	},
+  {
+    "j-hui/fidget.nvim",
+    tag = "legacy",
+    event = { "BufEnter" },
+    config = function()
+      -- Turn on LSP, formatting, and linting status and progress information
+      require("fidget").setup({
+        text = {
+          spinner = "dots_snake", -- Animation shown when tasks are ongoing.
+          done = "✔", -- Character shown when all tasks are complete.
+          commenced = "Started", -- Message shown when task starts.
+          completed = "Completed", -- Message shown when task completes.
+        },
+
+        align = {
+          bottom = true, -- Align fidgets along bottom edge of buffer.
+          right = true,  -- Align fidgets along right edge of buffer.
+        },
+
+        timer = {
+          spinner_rate = 125,  -- Frame rate of spinner animation, in ms.
+          fidget_decay = 2000, -- How long to keep around empty fidget, in ms.
+          task_decay = 1000,   -- How long to keep around completed task, in ms.
+        },
+
+        window = {
+          relative = "win", -- Where to anchor, either "win" or "editor".
+          blend = 0,        -- &winblend for the window.
+          zindex = nil,     -- The zindex value for the window.
+          border = "none",  -- Style of border for the fidget window.
+        },
+
+        fmt = {
+          leftpad = true,       -- Right-justify text in fidget box.
+          stack_upwards = true, -- List of tasks grows upwards.
+          max_width = 0,        -- Maximum width of the fidget box.
+
+          fidget = function(fidget_name, spinner)
+            return string.format("%s %s", spinner, fidget_name)
+          end,
+
+          task = function(task_name, message, percentage)
+            return string.format(
+              "%s%s [%s]",
+              message,
+              percentage and string.format(" (%s%%)", percentage) or "",
+              task_name
+            )
+          end,
+        },
+      })
+    end,
+  },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,12 +14,6 @@ return {
 
 			-- Progress/Status update for LSP
 			{ "j-hui/fidget.nvim", opts = {} },
-
-			-- Install lsp autocompletions
-			"hrsh7th/cmp-nvim-lsp",
-
-			-- Progress/Status update for LSP
-			{ "j-hui/fidget.nvim", opts = {} },
 		},
 		config = function()
 			local map_lsp_keybinds = require("user.keymaps").map_lsp_keybinds -- Has to load keymaps before pluginslsp
@@ -196,5 +190,48 @@ return {
 				lua = { "stylua" },
 			},
 		},
+	},
+	{
+		"pmizio/typescript-tools.nvim",
+		lazy = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		opts = {},
+		config = function()
+			-- |> Fetch default LSP preferences from `langserver-prefs.nvim`.
+			local lsp_prefs = require("langserver-prefs")
+
+			-- |> TS LSP Setup              ( STATE: Seems Good )
+			-- NOTE: Didn't configure many of the server specific things yet. Possible TODO.
+			require("typescript-tools").setup({
+				autostart = lsp_prefs.lsp_default_autostart,
+				flags = lsp_prefs.lsp_default_flags,
+				handlers = lsp_prefs.lsp_default_handlers,
+				capabilities = lsp_prefs.lsp_default_capabilities,
+				on_attach = lsp_prefs.lsp_default_on_attach,
+				single_file_support = lsp_prefs.lsp_default_singlefile_support,
+
+				settings = {
+					-- Spawn additional tsserver instance to calculate diagnostics on it.
+					separate_diagnostic_server = true,
+					-- "change" | "insert_leave": Determine when the client asks the server about diagnostic.
+					publish_diagnostic_on = "insert_leave",
+
+					tsserver_file_preferences = {
+						includeInlayParameterNameHints = "all",
+						-- includeCompletionsForModuleExports = true,
+						-- quotePreference = "auto",
+					},
+
+					-- Specify a list of plugins to load by tsserver, e.g., for support `styled-components`
+					-- (see 💅 `styled-components` support section)
+					tsserver_plugins = {},
+					tsserver_format_options = {},
+					tsserver_file_preferences = {},
+				},
+			})
+		end,
 	},
 }
