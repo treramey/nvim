7e2b8967f13e1389803f9abee14635d1fedc6394
Trevor Ramey <treramey@gmail.com>
2024-07-23 16:15:01 -0500

    Update

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -157,26 +157,26 @@ return {
 				opts = {},
 			},
 		},
-	        -- stylua: ignore
-	        keys = {
-	            { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
-	            { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
-	            { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
-	            { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
-	            { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
-	            { "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
-	            { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
-	            { "<leader>dj", function() require("dap").down() end, desc = "Down" },
-	            { "<leader>dk", function() require("dap").up() end, desc = "Up" },
-	            { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
-	            { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
-	            { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
-	            { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
-	            { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
-	            { "<leader>ds", function() require("dap").session() end, desc = "Session" },
-	            { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
-	            { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
-	        },
+    -- stylua: ignore
+    keys = {
+        { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+        { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+        { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+        { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+        { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+        { "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
+        { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+        { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+        { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+        { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+        { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+        { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+        { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+        { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+        { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+        { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+        { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+    },
 		config = function(_, _)
 			local status, dap = pcall(require, "dap")
 			if not status then
@@ -191,20 +191,40 @@ return {
 
 			dap.adapters.coreclr = {
 				type = "executable",
-				command = vim.fn.stdpath("data") .. "/mason/bin/netcoredbg",
+				command = vim.fn.stdpath("data") .. "/lazy/netcoredbg-macOS-arm64.nvim/netcoredbg",
 				args = { "--interpreter=vscode" },
+				options = {
+					detached = false, -- Will put the output in the REPL. #CloseEnough
+					cwd = dotnet_ph.GetDebugCwd(),
+				},
 			}
+
+			-- Neotest Test runner looks at this table
+			dap.adapters.netcoredbg = dap.adapters.coreclr
+
+			-- useful for debugging issues with dap
+			-- Logs are written to :lua print(vim.fn.stdpath('cache'))
+			-- dap.set_log_level('DEBUG')  -- or `TRACE` for more logs
 
 			dap.configurations.cs = {
 				{
 					type = "coreclr",
 					name = "launch - netcoredbg",
 					request = "launch",
-					program = function()
-						return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
-					end,
+					-- program = dotnet_ph.GetDllPath,
+					console = "integratedTerminal",
+					-- program = function()
+					-- 	return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+					-- end,
 				},
 			}
+		end,
+	},
+	{
+		"Cliffback/netcoredbg-macOS-arm64.nvim",
+		dependencies = { "mfussenegger/nvim-dap" },
+		config = function()
+			require("netcoredbg-macOS-arm64").setup(require("dap"))
 		end,
 	},
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -128,6 +128,29 @@ return {
 				},
 				marksman = {},
 				ocamllsp = {},
+				omnisharp = {
+					handlers = {
+						["textDocument/definition"] = function(...)
+							return require("omnisharp_extended").handler(...)
+						end,
+					},
+					keys = {
+						{
+							"gd",
+							function()
+								require("omnisharp_extended").telescope_lsp_definitions()
+							end,
+							desc = "Goto Definition",
+						},
+					},
+					cmd = { vim.fn.stdpath("data") .. "/mason/packages/omnisharp/omnisharp" },
+					analyze_open_documents_only = false,
+					enable_ms_build_load_projects_on_demand = false,
+					enable_roslyn_analyzers = false,
+					organize_imports_on_format = true,
+					enable_import_completion = true,
+					sdk_include_prereleases = true,
+				},
 				nil_ls = {},
 				pyright = {},
 				sqlls = {},
@@ -242,5 +265,5 @@ return {
 	-- 		})
 	-- 	end,
 	-- },
-	-- { "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
+	{ "Hoffs/omnisharp-extended-lsp.nvim", lazy = true },
 }
