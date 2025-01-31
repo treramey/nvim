1ec8129959c5865fc38bed9d438a0c9ef63a7cac
Trevor Ramey <treramey@gmail.com>
2025-01-31 10:12:42 -0600

    WIP

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -49,4 +49,23 @@ return {
 			},
 		},
 	},
+	{
+		"echasnovski/mini.icons",
+		opts = {
+			file = {
+				["init.lua"] = { glyph = "󰢱", hl = "MiniIconsAzure" },
+			},
+			lsp = {
+				copilot = { glyph = "", hl = "MiniIconsOrange" },
+				snippet = { glyph = "" },
+			},
+		},
+		lazy = true,
+		init = function()
+			package.preload["nvim-web-devicons"] = function()
+				require("mini.icons").mock_nvim_web_devicons()
+				return package.loaded["nvim-web-devicons"]
+			end
+		end,
+	},
 }
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -20,69 +20,6 @@ return {
 		opts = {
 			bigfile = { enabled = true },
 			dim = { enabled = true },
-			dashboard = {
-				formats = {
-					key = function(item)
-						return { { "[", hl = "function" }, { item.key, hl = "key" }, { "]", hl = "function" } }
-					end,
-					header = { "%s", align = "center", hl = "MiniIconsBlue" },
-					icon = function(item)
-						if item.file and item.icon == "file" or item.icon == "directory" then
-							return Snacks.dashboard.icon(item.file, item.icon)
-						end
-						return { item.icon, width = 2, hl = "MiniIconsPurple" }
-					end,
-				},
-				preset = {
-					keys = {
-						{
-							icon = " ",
-							key = "f",
-							desc = "find file",
-							action = ":lua Snacks.dashboard.pick('files')",
-						},
-						{
-							icon = " ",
-							key = "w",
-							desc = "find text",
-							action = ":lua Snacks.dashboard.pick('live_grep')",
-						},
-						{
-							icon = " ",
-							key = "r",
-							desc = "recent files",
-							action = ":lua Snacks.dashboard.pick('oldfiles')",
-						},
-						{ icon = " ", key = "e", desc = "explorer", action = ":lua require('oil').toggle_float()" },
-						{ icon = " ", key = "g", desc = "browse git", action = ":lua Snacks.lazygit()" },
-						{ icon = "󰒲 ", key = "l", desc = "lazy", action = ":Lazy" },
-						{ icon = "󱌣 ", key = "m", desc = "mason", action = ":Mason" },
-						{ icon = "󰭿 ", key = "q", desc = "quit", action = ":qa" },
-					},
-				},
-				sections = {
-					{
-						section = "terminal",
-						cmd = "chafa /Users/trevor/Downloads/1332280.jpeg --format symbols --symbols vhalf --size 60x17 --stretch; sleep .1",
-						height = 17,
-						padding = 1,
-					},
-					{
-						pane = 2,
-						{ title = "shortcuts", hl = "" },
-						{ section = "keys", padding = 1 },
-						{ title = "mru ", file = vim.fn.fnamemodify(".", ":~"), padding = 1 },
-						{ section = "recent_files", cwd = true, limit = 5, padding = 1 },
-						{ section = "startup" },
-					},
-				},
-			},
-			git = { enabled = true },
-			gitbrowse = { enabled = true },
-			lazygit = {
-				configure = false,
-				theme_path = vim.fs.normalize(vim.fn.expand("~/.config/lazygit/config.yml")),
-			},
 			indent = { enabled = true },
 			notifier = {
 				enabled = true,
