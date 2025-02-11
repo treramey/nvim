bf6186fcfbc0a29ba21cc89d713bef88ecd901c7
Trevor Ramey <hello@trevors.email>
2025-02-10 20:17:26 -0500

    WIP

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -84,3 +84,57 @@ vim.opt.guicursor = {
 	"a:blinkwait700-blinkoff400-blinkon250", -- All modes: blinking settings
 	"sm:block-blinkwait175-blinkoff150-blinkon175", -- Showmatch: block cursor with specific blinking settings
 }
+
+-- This forces the use of OSC 52 which allows Neovim to 'forward' copy and paste
+-- commands via the terminal.
+--
+-- What this means is, whether you're in Neovim on a remote server,
+-- copy and paste commands will be driven thru your terminal emulator
+-- and thus, into your host system's clipboard.
+-- -- TMUX documentation about its clipboard - https://github.com/tmux/tmux/wiki/Clipboard#the-clipboard
+
+local is_tmux_session = vim.env.TERM_PROGRAM == "tmux" -- Tmux is its own clipboard provider which directly works.
+if vim.env.SSH_TTY then
+	local osc52 = require("vim.ui.clipboard.osc52")
+	vim.g.clipboard = {
+		name = "OSC 52",
+		copy = {
+			["+"] = osc52.copy("+"),
+			["*"] = osc52.copy("*"),
+		},
+		paste = {
+			["+"] = osc52.paste("+"),
+			["*"] = osc52.paste("*"),
+		},
+	}
+end
+
+-- If we  are running inside ssh AND we are in a tmux sesssion, OSC 52 won't
+-- work, instead we can use tmux's native copy/paste functionality.
+if vim.env.SSH_TTY and is_tmux_session then
+	local copy = { "tmux", "load-buffer", "-w", "-" }
+	local paste = { "bash", "-c", "tmux refresh-client -l && sleep 0.05 && tmux save-buffer -" }
+	vim.g.clipboard = {
+		name = "tmux",
+		copy = {
+			["+"] = copy,
+			["*"] = copy,
+		},
+		paste = {
+			["+"] = paste,
+			["*"] = paste,
+		},
+		cache_enabled = 0,
+	}
+end
+
+-- Creating a new comment string after 'o' is annoying, enforce it never happens
+-- but ensure it does happen on new lines (r option).
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = "*",
+	command = "set formatoptions-=o",
+})
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = "*",
+	command = "set formatoptions+=r",
+})
