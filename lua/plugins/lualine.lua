b6f3af3dcc4baa2649cd3fd4e53c7323fdb0c156
Trevor Ramey <hello@trevors.email>
2025-02-23 17:56:46 -0600

    WIP

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-lualine/lualine.nvim",
 		event = "VeryLazy",
 		config = function()
-			local harpoon = require("harpoon.mark")
+			local harpoon = require("harpoon")
 
 			local function truncate_branch_name(branch)
 				if not branch or branch == "" then
@@ -21,22 +21,22 @@ return {
 				end
 			end
 
-			local function harpoon_component()
-				local total_marks = harpoon.get_length()
-
-				if total_marks == 0 then
-					return ""
-				end
-
-				local current_mark = "—"
-
-				local mark_idx = harpoon.get_current_index()
-				if mark_idx ~= nil then
-					current_mark = tostring(mark_idx)
-				end
-
-				return string.format("󱡅 %s/%d", current_mark, total_marks)
-			end
+			-- local function harpoon_component()
+			-- 	local total_marks = harpoon:list():length()
+			--
+			-- 	if total_marks == 0 then
+			-- 		return ""
+			-- 	end
+			--
+			-- 	local current_mark = "—"
+			--
+			-- 	local mark_idx = harpoon:list().get_current_index()
+			-- 	if mark_idx ~= nil then
+			-- 		current_mark = tostring(mark_idx)
+			-- 	end
+			--
+			-- 	return string.format("󱡅 %s/%d", current_mark, total_marks)
+			-- end
 
 			require("lualine").setup({
 				options = {
@@ -48,7 +48,7 @@ return {
 				sections = {
 					lualine_b = {
 						{ "branch", icon = "", fmt = truncate_branch_name },
-						harpoon_component,
+						-- harpoon_component,
 						"diff",
 						"diagnostics",
 					},
